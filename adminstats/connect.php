8bfae4ed960234055d4bc4fd0ecbe8838ae862e4
kingi001 <kingikhamis518@gmail.com>
2022-06-13 15:42:48 +0300

    connect.php adited

--- adminstats/connect.php
+++ adminstats/connect.php
@@ -1,5 +1,5 @@
 <?php
-$dsn='mysql:host=localhost;dbname=blog_admin_db';//dbname and host
-$username='root';//username
-$password='';//password
+$dsn='mysql:host=localhost;dbname=transformation_blog_admin_db';//dbname and host
+$username='transformation';//username
+$password='rosedee2022#';//password
 ?>
